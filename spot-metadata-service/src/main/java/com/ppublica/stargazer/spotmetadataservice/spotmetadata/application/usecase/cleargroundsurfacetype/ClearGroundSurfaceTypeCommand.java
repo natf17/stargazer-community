2feff56ddad1f538a53387f8985dbce2c99f5807
natf17 <natfar.dev@gmail.com>
2026-02-04 00:09:30 -0500

    Implement ClearGroundSurfaceType use case

---
+++ spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/spotmetadata/application/usecase/cleargroundsurfacetype/ClearGroundSurfaceTypeCommand.java
@@ -0,0 +1,5 @@
+package com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.cleargroundsurfacetype;
+
+import com.ppublica.stargazer.spotmetadataservice.spotmetadata.domain.model.SpotMetadataId;
+
+public record ClearGroundSurfaceTypeCommand(SpotMetadataId id) { }
---
+++ spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/spotmetadata/application/usecase/cleargroundsurfacetype/ClearGroundSurfaceTypeUseCase.java
@@ -0,0 +1,5 @@
+package com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.cleargroundsurfacetype;
+
+public interface ClearGroundSurfaceTypeUseCase {
+    void handle(ClearGroundSurfaceTypeCommand command);
+}
