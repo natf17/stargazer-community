6b888b7291b48297671bd51ad1220089dcee3649
natf17 <natfar.dev@gmail.com>
2026-02-04 00:04:51 -0500

    Implement ClearSkyVisibilityBucket use case

---
+++ spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/spotmetadata/application/usecase/clearskyvisibilitybucket/ClearSkyVisibilityBucketCommand.java
@@ -0,0 +1,5 @@
+package com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.clearskyvisibilitybucket;
+
+import com.ppublica.stargazer.spotmetadataservice.spotmetadata.domain.model.SpotMetadataId;
+
+public record ClearSkyVisibilityBucketCommand(SpotMetadataId id) {}
---
+++ spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/spotmetadata/application/usecase/clearskyvisibilitybucket/ClearSkyVisibilityBucketUseCase.java
@@ -0,0 +1,5 @@
+package com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.clearskyvisibilitybucket;
+
+public interface ClearSkyVisibilityBucketUseCase {
+    void handle(ClearSkyVisibilityBucketCommand command);
+}
