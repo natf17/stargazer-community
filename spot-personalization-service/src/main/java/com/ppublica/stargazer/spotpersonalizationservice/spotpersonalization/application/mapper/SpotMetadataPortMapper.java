a249ae5b6f727d2f52477a4e019cb402f5142d8d
natf17 <natfar.dev@gmail.com>
2026-02-09 23:13:32 -0500

    Implement SpotMetadataPortMapper

--- spot-personalization-service/src/main/java/com/ppublica/stargazer/spotpersonalizationservice/spotpersonalization/application/mapper/SpotMetadataPortMapper.java
+++ spot-personalization-service/src/main/java/com/ppublica/stargazer/spotpersonalizationservice/spotpersonalization/application/mapper/SpotMetadataPortMapper.java
@@ -1,10 +1,28 @@
 package com.ppublica.stargazer.spotpersonalizationservice.spotpersonalization.application.mapper;
 
+import com.ppublica.stargazer.sharedkernelspot.SpotId;
+import com.ppublica.stargazer.sharedkernelspotmetadata.SpotMetadataId;
 import com.ppublica.stargazer.spotpersonalizationservice.spotpersonalization.application.domain.spotmetadata.SpotMetadata;
 import com.ppublica.stargazer.spotpersonalizationservice.spotpersonalization.application.port.spotmetadata.SpotMetadataPortDto;
 
+import java.util.Optional;
+import java.util.Set;
+
 public class SpotMetadataPortMapper {
     public SpotMetadata toSpotMetadata(SpotMetadataPortDto spotMetadataPortDto) {
-        throw new UnsupportedOperationException();
+        return new SpotMetadata(SpotId.of(spotMetadataPortDto.spotId()),
+                SpotMetadataId.of(spotMetadataPortDto.id()),
+                Optional.of(spotMetadataPortDto.description()),
+                spotMetadataPortDto.name(),
+                Set.copyOf(spotMetadataPortDto.accessPolicies()),
+                Set.copyOf(spotMetadataPortDto.accessibilities()),
+                Set.copyOf(spotMetadataPortDto.amenities()),
+                Optional.of(spotMetadataPortDto.terrainInclination()),
+                Optional.of(spotMetadataPortDto.groundSurfaceType()),
+                Optional.of(spotMetadataPortDto.skyVisibilityBucket()),
+                Optional.of(spotMetadataPortDto.visibilityCeiling()),
+                Optional.of(spotMetadataPortDto.globalHorizonVisibility()),
+                Optional.of(spotMetadataPortDto.bestHorizonDirection())
+                );
     }
 }
--- spot-personalization-service/src/main/java/com/ppublica/stargazer/spotpersonalizationservice/spotpersonalization/application/port/spotmetadata/SpotMetadataPortDto.java
+++ spot-personalization-service/src/main/java/com/ppublica/stargazer/spotpersonalizationservice/spotpersonalization/application/port/spotmetadata/SpotMetadataPortDto.java
@@ -1,4 +1,18 @@
 package com.ppublica.stargazer.spotpersonalizationservice.spotpersonalization.application.port.spotmetadata;
 
-public record SpotMetadataPortDto() {
+import java.util.Set;
+
+public record SpotMetadataPortDto(String id,
+                                  String spotId,
+                                  String description,
+                                  String name,
+                                  Set<String> accessPolicies,
+                                  Set<String> accessibilities,
+                                  Set<String> amenities,
+                                  String terrainInclination,
+                                  String groundSurfaceType,
+                                  String skyVisibilityBucket,
+                                  double visibilityCeiling,
+                                  String globalHorizonVisibility,
+                                  String bestHorizonDirection) {
 }
