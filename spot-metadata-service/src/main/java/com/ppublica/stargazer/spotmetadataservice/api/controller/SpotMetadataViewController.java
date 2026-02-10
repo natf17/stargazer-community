d5c80708248145b91b1561a859f0ca9585a649f5
natf17 <natfar.dev@gmail.com>
2026-02-10 12:56:11 -0500

    Implement api endpoint for GetSpotMetadataViewsBySpotIds use case

--- spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/api/controller/SpotMetadataViewController.java
+++ spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/api/controller/SpotMetadataViewController.java
@@ -1,18 +1,24 @@
 package com.ppublica.stargazer.spotmetadataservice.api.controller;
 
+import com.ppublica.stargazer.spotmetadataservice.api.dto.SpotMetadataListRequest;
 import com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.dto.SpotMetadataListResponse;
 import com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.dto.SpotMetadataView;
 import com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.getspotmetadataviewbyspot.GetSpotMetadataViewBySpotQuery;
 import com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.getspotmetadataviewbyspot.GetSpotMetadataViewBySpotUseCase;
+import com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.getspotmetadataviewsbyspotids.GetSpotMetadataViewsBySpotIdsQuery;
+import com.ppublica.stargazer.spotmetadataservice.spotmetadata.application.usecase.getspotmetadataviewsbyspotids.GetSpotMetadataViewsBySpotIdsUseCase;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/spot-metadata")
 public class SpotMetadataViewController {
     private final GetSpotMetadataViewBySpotUseCase getSpotMetadataViewBySpotUseCase;
+    private final GetSpotMetadataViewsBySpotIdsUseCase getSpotMetadataViewsBySpotIdsUseCase;
 
-    public SpotMetadataViewController(GetSpotMetadataViewBySpotUseCase getSpotMetadataViewBySpotUseCase) {
+    public SpotMetadataViewController(GetSpotMetadataViewBySpotUseCase getSpotMetadataViewBySpotUseCase,
+                                      GetSpotMetadataViewsBySpotIdsUseCase getSpotMetadataViewsBySpotIdsUseCase) {
         this.getSpotMetadataViewBySpotUseCase = getSpotMetadataViewBySpotUseCase;
+        this.getSpotMetadataViewsBySpotIdsUseCase = getSpotMetadataViewsBySpotIdsUseCase;
     }
 
     @GetMapping("/by-spot/{id}")
@@ -21,7 +27,7 @@ public class SpotMetadataViewController {
     }
 
     @PostMapping("/by-spot")
-    public SpotMetadataListResponse getMetadataViewsBySpotIds(@RequestBody SpotMetadataListResponse ids) {
-        return ;
+    public SpotMetadataListResponse getMetadataViewsBySpotIds(@RequestBody SpotMetadataListRequest request) {
+        return getSpotMetadataViewsBySpotIdsUseCase.execute(new GetSpotMetadataViewsBySpotIdsQuery(request.spotIds()));
     }
 }
---
+++ spot-metadata-service/src/main/java/com/ppublica/stargazer/spotmetadataservice/api/dto/SpotMetadataListRequest.java
@@ -0,0 +1,5 @@
+package com.ppublica.stargazer.spotmetadataservice.api.dto;
+
+import java.util.List;
+
+public record SpotMetadataListRequest(List<String> spotIds) {}
